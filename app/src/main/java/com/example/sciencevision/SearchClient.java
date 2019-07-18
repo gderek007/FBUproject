d4177199eb5a03f2b3ef93f4f204a4212fbd5b01
Derek Garcia <39495257+gderek007@users.noreply.github.com>
2019-07-18 15:13:47 -0700

    Update SearchClient.java
    "Able to get the first sentence of the search label and can intake strings to search on wikipedia."

--- app/src/main/java/com/example/sciencevision/SearchClient.java
+++ app/src/main/java/com/example/sciencevision/SearchClient.java
@@ -6,21 +6,17 @@ import android.util.Log;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
-
+import org.json.JSONException;
 import org.json.JSONObject;
-
 import cz.msebera.android.httpclient.Header;
 
 public class SearchClient {
-
     private AsyncHttpClient client;
     private String API_BASE_URL = "https://simple.wikipedia.org/w/api.php";
     public SearchClient() {
         client = new AsyncHttpClient();
     }
-
-
-    public void getWiki() {
+    public void getWiki(String searchLabel) {
         // create the url
         String url = API_BASE_URL;
 
@@ -32,21 +28,25 @@ public class SearchClient {
         params.put("exintro","");
         params.put("explaintext","");
         params.put("redirects","1");
-        params.put("titles","dog");
+        params.put("titles",searchLabel);
 
         //execute a GET request, expect JSON object response
         client.get(url, params, new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                Log.d("SearchClient", response.toString());
-//                try {
-////                    response.getJSONObject("query").getJSONObject("pages").getJSONString("285").getString("extract");
-//                    Log.d("SearchClient", response.toString());
-//                }
-//                catch(JSONException e)
-//                {
-//                    e.printStackTrace();
-//                }
+                try {
+                    //Gets the page number for the search
+                    String page=response.getJSONObject("query").getJSONObject("pages").names().get(0).toString();
+                    //Description of what the searchLabel
+                    String finalresponse=response.getJSONObject("query").getJSONObject("pages").getJSONObject(page).getString("extract");
+                    //Easy Solution for grabbing the first sentence
+                    String firstsentence=finalresponse.substring(0,finalresponse.indexOf(".")+1);
+                    Log.d("Search Client",firstsentence);
+                }
+                catch(JSONException e)
+                {
+                    e.printStackTrace();
+                }
             }
 
             @Override
@@ -54,7 +54,6 @@ public class SearchClient {
                 super.onFailure(statusCode, headers, throwable, errorResponse);
             }
         });
-
     }
 
 
